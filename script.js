8ed8999515ca42ed777429d769d6db0c665fa9bc
rahul-sahadevan <rahulsahadevan000@gmail.com>
2023-09-04 09:51:29 +0530

    new

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const button = document.getElementById("btn");
 
 
 function mapNavigator(){
-    window.location.href = "http://127.0.0.1:5500/map.html";
-    return false;
+    window.location.href = "http://127.0.0.1:5500/map.html",true;
 }
 
 // adding event listner to the buttin fetchweather
