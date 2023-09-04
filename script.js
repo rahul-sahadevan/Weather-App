35202292671f4a59f9b0b62f65fd242680adecab
rahul-sahadevan <rahulsahadevan000@gmail.com>
2023-09-04 12:37:19 +0530

    error

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const button = document.getElementById("btn");
 async function mapNavigator(){
     try{
 
-        window.location.href = "http://127.0.0.1:5500/map.html";
+        document.location = "http://127.0.0.1:5500/map.html";
     }
     catch(error){
         console.log(error);
